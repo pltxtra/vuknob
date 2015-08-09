2c96ed21c1b8d0728c0d917052f56913099809bc
Anton Persson <don.juanton@gmail.com>
2015-08-10 00:06:27 +0200

    Removed autogenerated files from git.

--- Applications/vuKNOB/Android/src/com/holidaystudios/satan/AutogenDate.java
+++
@@ -1,7 +0,0 @@
-package com.holidaystudios.satan;
-
-public class AutogenDate
-{
-    public final static String DATE = "timestamp1439157134.file";
-}
-
--- Applications/vuKNOBnet/Android/src/com/holidaystudios/vuknob/AutogenDate.java
+++
@@ -1,7 +0,0 @@
-package com.holidaystudios.vuknob;
-
-public class AutogenDate
-{
-    public final static String DATE = "timestamp1439157141.file";
-}
-
